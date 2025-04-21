d6961cad410359452cc5951d7fb09ae3f8a62b59
mzsellem <marleysellem@gmail.com>
2025-04-21 11:54:01 -0600

    basic header and footer for home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <header className='w-full'>
         <nav>Sticky Navbar</nav>
       </header>
       <main>
         <p>Your page content goes here.</p>
       </main>
-      <footer>
+      <footer className='w-full'>
         <p>Sticky Footer</p>
       </footer>
     </div>
