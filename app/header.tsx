d41008cb22716002d39bce9d44d56349623e78b9
mzsellem <marleysellem@gmail.com>
2025-04-21 12:38:36 -0600

    created header component

---
+++ app/header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+    <header className='w-full'>
+            <nav>
+            <ul className='flex justify-around'>
+                <li><a href="#Home">Home</a></li>
+                <li><a href="#About">About</a></li>
+                <li><a href="#Media">Media</a></li>
+                <li><a href="#Engagements">Engagements</a></li>
+                <li><a href="#Contact">Contact</a></li>
+            </ul>
+            </nav>
+    </header>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
+import Header from './header';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <header className='w-full'>
-        <nav>
-          <ul className='flex justify-around'>
-            <li><a href="#Home">Home</a></li>
-            <li><a href="#About">About</a></li>
-            <li><a href="#Media">Media</a></li>
-            <li><a href="#Engagements">Engagements</a></li>
-            <li><a href="#Contact">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
       <main>
         <p>Your page content goes here.</p>
       </main>
