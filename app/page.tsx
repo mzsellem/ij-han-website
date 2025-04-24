ee5efed45f2bd8ce55e408c9267fa7264c526a07
mzsellem <marleysellem@gmail.com>
2025-04-24 09:59:22 -0600

    add images, prevent chain-scrolling, and format main

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <HomeNavbar />
-      <main>
-        <p>Your page content goes here.</p>
+      <main className='flex flex-col items-center justify-center'>
+        <div>
+          <img src="ij-serious-conductor.png"></img>
+          <img src="ij-holds-piano.png"></img>
+          <img src="ij-baton-head.png"></img>
+        </div>
       </main>
       <footer className='w-full'>
         <ul className='flex justify-around'>
