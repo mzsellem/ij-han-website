cbf0932ccc14dc51cf583bd7c4c3fddcc2e57c3d
mzsellem <marleysellem@gmail.com>
2025-04-28 14:46:42 -0600

    adjust navbar and main content formatting

--- app/home-navbar.tsx
+++ app/home-navbar.tsx
@@ -4,8 +4,8 @@ export default function HomeNavbar() {
   return (
     <header className="w-full bg-gray-200 p-4">
       <nav className="flex flex-col sm:flex-col items-center">
-        <h1 className="text-2xl sm:text-3xl mb-2">Justin "IJ" Han</h1>
-        <ul className="flex flex-row sm:flex-row justify-center sm:justify-end sm:space-y-0 sm:space-x-10 space-x-9">
+        <h1 className="text-3xl sm:text-4xl mb-2">Justin "IJ" Han</h1>
+        <ul className="flex flex-row sm:flex-row justify-center sm:justify-end sm:space-y-0 sm:space-x-10 space-x-2">
           <li><a href="#Home" className="hover:underline">Home</a></li>
           <li><a href="#About" className="hover:underline">About</a></li>
           <li><a href="#Media" className="hover:underline">Media</a></li>
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <HomeNavbar />
-      <main className="flex flex-col items-center justify-center mt-8"> {/* Added margin-top */}
-        <div className="ml-12 mr-12 mb-12 space-y-8">
+      <main className="flex flex-col sm:flex-row items-center justify-center mt-8"> {/* Added margin-top */}
+        <div className="ml-12 mr-12 mb-8 space-y-8">
           <img src="ij-serious-baton.png" alt="Serious Baton" className="rounded-2xl"/>
           <img src="ij-hold-piano.png" alt="Hold Piano" className="rounded-2xl" />
           <img src="ij-baton-head.png" alt="Baton Head" className="rounded-2xl" />
