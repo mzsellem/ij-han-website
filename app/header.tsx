9abda1854d45ac2d75c9a5edd9f8d12d5e1baf99
mzsellem <marleysellem@gmail.com>
2025-04-28 14:37:13 -0600

    format photos for mobile

--- app/header.tsx
+++ app/header.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Navbar() {
     return (
-    <header className='w-full'>
+    <header className="w-full">
             <nav>
-            <ul className='flex justify-around'>
+            <ul className="flex justify-around">
                 <li><a href="#Home">Home</a></li>
                 <li><a href="#About">About</a></li>
                 <li><a href="#Media">Media</a></li>
--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
     <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <HomeNavbar />
       <main className="flex flex-col items-center justify-center mt-8"> {/* Added margin-top */}
-        <div>
-          <img src="ij-serious-baton.png" alt="Serious Baton" />
-          <img src="ij-hold-piano.png" alt="Hold Piano" />
-          <img src="ij-baton-head.png" alt="Baton Head" />
+        <div className="ml-12 mr-12 mb-12 space-y-8">
+          <img src="ij-serious-baton.png" alt="Serious Baton" className="rounded-2xl"/>
+          <img src="ij-hold-piano.png" alt="Hold Piano" className="rounded-2xl" />
+          <img src="ij-baton-head.png" alt="Baton Head" className="rounded-2xl" />
         </div>
       </main>
       <footer className="w-full">
