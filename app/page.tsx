052254d74305f4e2c9d2b2b05389e577a2bf5789
mzsellem <marleysellem@gmail.com>
2025-05-05 13:33:54 -0600

    remove comment

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <HomeNavbar />
-      <main className="flex flex-col sm:flex-row items-center justify-center mt-8"> {/* Added margin-top */}
+      <main className="flex flex-col sm:flex-row items-center justify-center mt-8">
         <div className="ml-12 mr-12 mb-8 space-y-8">
           <img src="ij-serious-baton.png" alt="Serious Baton" className="rounded-2xl"/>
           <img src="ij-hold-piano.png" alt="Hold Piano" className="rounded-2xl" />
