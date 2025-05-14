e752c6b92e6c22a3e38415ba9d849a474ca3f47d
mzsellem <marleysellem@gmail.com>
2025-05-14 13:04:24 -0600

    add span around Link content

--- app/Components/home-navbar.tsx
+++ app/Components/home-navbar.tsx
@@ -9,11 +9,11 @@ export default function HomeNavbar() {
       <nav className="flex flex-col sm:flex-row items-center sm:justify-between ml-5 mr-5">
         <h1 className="text-3xl sm:text-5xl p-2">Justin "IJ" Han</h1>
         <ul className="flex flex-row sm:flex-row justify-center sm:justify-end sm:space-x-10 space-x-6 text-xl">
-          <li><Link href="/" className="hover:underline">Home</Link></li>
-          <li><Link href="/about" className="hover:underline">About</Link></li>
-          <li><Link href="/media" className="hover:underline">Media</Link></li>
-          <li><Link href="/engagements" className="hover:underline">Engagements</Link></li>
-          <li><Link href="/contact" className="hover:underline">Contact</Link></li>
+          <li><Link href="/"><span className="hover:underline">Home</span></Link></li>
+          <li><Link href="/about"><span className="hover:underline">About</span></Link></li>
+          <li><Link href="/media"><span className="hover:underline">Media</span></Link></li>
+          <li><Link href="/engagements"><span className="hover:underline">Engagements</span></Link></li>
+          <li><Link href="/contact"><span className="hover:underline">Contact</span></Link></li>
           
         </ul>
       </nav>
