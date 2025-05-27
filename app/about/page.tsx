ac055953a1814198d4ab75842453061cc464eaec
mzsellem <marleysellem@gmail.com>
2025-05-27 17:14:26 -0600

    make navbar span the viewport for all pages except home

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Navbar from '../Components/header'
+import Navbar from '../Components/navbar'
 
 export default function AboutPage() {
     return (
-      <main className="p-8">
+      <main>
         <Navbar/>
         <h1 className="text-3xl font-bold">About Me</h1>
         <p>This is the About page.</p>
--- app/media/page.tsx
+++ app/media/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Navbar from '../Components/header'
+import Navbar from '../Components/navbar'
 
 export default function MediaPage() {
     return (
-      <main className="p-8">
+      <main>
         <Navbar/>
         <h1 className="text-3xl font-bold">Media</h1>
         <p>This is the Media page.</p>
