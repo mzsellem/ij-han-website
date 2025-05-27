8acfe3147f63ce5eb8f09bcfd311339285a5a0ed
mzsellem <marleysellem@gmail.com>
2025-05-27 17:49:59 -0600

    remove unused images and add rounded social media icons

--- app/Components/footer.tsx
+++ app/Components/footer.tsx
@@ -4,11 +4,12 @@ import Image from 'next/image';
 export default function Footer() {
     return (
 <footer className="w-full">
-<ul className="flex justify-center space-x-2 text-xl">
+  <h1 className="font-bold">Connect</h1>
+<ul className="flex justify-center space-x-4 text-xl">
   <li>
     <a href="https://www.facebook.com/maestro.han2">
       <Image
-      src="/fb-icon.svg"
+      src="/fb-round.svg"
       alt="Facebook Icon"
       width={40}
       height={40}
@@ -19,7 +20,7 @@ export default function Footer() {
   <li>
     <a href="https://www.instagram.com/maestro.han/?fbclid=IwY2xjawKjLBZleHRuA2FlbQIxMABicmlkETFjSEViMnFsdHNKN2tNVnJrAR4g2iIz6NR8NQ24tUweAcIyJXjEXojYSeoba3bEFn1DH3i7uYhsd0S49dMXgA_aem_pcww02QSUE0ix4HMOnvKdA#">
       <Image
-      src="/insta-2.svg"
+      src="/insta-round.svg"
       alt="Facebook Icon"
       width={40}
       height={40}
@@ -30,7 +31,7 @@ export default function Footer() {
   <li>
     <a href="https://www.linkedin.com/in/jhan719/">
       <Image
-      src="/linkedin.svg"
+      src="/linkedin-round.svg"
       alt="Facebook Icon"
       width={40}
       height={40}
