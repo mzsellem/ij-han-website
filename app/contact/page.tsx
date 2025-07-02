202b11039a08b8e6d1a4d5ea44ab33748399accc
mzsellem <marleysellem@gmail.com>
2025-07-02 09:41:20 -0600

    contact page stub up

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,11 +4,23 @@ import Footer from '../Components/footer';
 
 export default function ContactPage() {
     return (
-      <main>
+      <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
         <Navbar/>
-        <h1 className="text-3xl font-bold">Contact Page</h1>
-        <p>This is the Contact page.</p>
+        <main className="sm:w-2/3 w-full flex flex-col justify-center items-center gap-y-4 text-xl mt-16">
+          <h1 className="text-3xl font-bold mb-4 border-b-1 border-white w-full pb-4 inline-block text-center">
+            Get in Touch
+          </h1>
+          <div className="sm:w-1/2 w-full overflow-hidden relative rounded-2xl">
+            <img
+              src="ij-chill.png"
+              alt="Relax Picture of Justin"
+              className={`w-full h-full object-cover rounded-2xl"
+              }`}
+            />
+        </div>
+        <h1 className="mb-4">jhan719@live.com</h1>
+        </main>
         <Footer/>
-      </main>
+      </div>
     );
   }
