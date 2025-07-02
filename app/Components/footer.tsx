e822f62165004b6ff5ed73f676434ed8353ff559
mzsellem <marleysellem@gmail.com>
2025-07-02 12:28:56 -0600

    add franchise approved logos to footer and format

--- app/Components/footer.tsx
+++ app/Components/footer.tsx
@@ -5,11 +5,22 @@ export default function Footer() {
     return (
 <footer className="w-full">
   <h1 className="font-bold mb-2">Connect</h1>
-<ul className="flex justify-center space-x-4 text-xl">
+<ul className="flex justify-center items-center space-x-4 text-xl">
+  <li>
+    <a href="https://www.youtube.com/@justinhan7547">
+      <Image
+      src="/yt-logo.png"
+      alt="Youtube Icon"
+      width={80}
+      height={80}
+      style={{ objectFit: 'cover' }}
+      />
+    </a>
+  </li>
   <li>
     <a href="https://www.facebook.com/maestro.han2">
       <Image
-      src="/fb-round.svg"
+      src="/fb-logo.png"
       alt="Facebook Icon"
       width={30}
       height={30}
@@ -20,7 +31,7 @@ export default function Footer() {
   <li>
     <a href="https://www.instagram.com/maestro.han/?fbclid=IwY2xjawKjLBZleHRuA2FlbQIxMABicmlkETFjSEViMnFsdHNKN2tNVnJrAR4g2iIz6NR8NQ24tUweAcIyJXjEXojYSeoba3bEFn1DH3i7uYhsd0S49dMXgA_aem_pcww02QSUE0ix4HMOnvKdA#">
       <Image
-      src="/insta-round.svg"
+      src="/insta-logo.png"
       alt="Facebook Icon"
       width={30}
       height={30}
@@ -31,10 +42,10 @@ export default function Footer() {
   <li>
     <a href="https://www.linkedin.com/in/jhan719/">
       <Image
-      src="/linkedin-round.svg"
+      src="/li-logo.png"
       alt="Facebook Icon"
-      width={30}
-      height={30}
+      width={80}
+      height={80}
       style={{ objectFit: 'cover' }}
       />
     </a>
