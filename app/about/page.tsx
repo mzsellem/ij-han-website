64850d8c44829b1402520c5073bc8a7f8188fcda
mzsellem <marleysellem@gmail.com>
2025-07-02 16:13:17 -0600

    insert content and further formatting for about me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,12 +6,19 @@ export default function AboutPage() {
     return (
       <div className="min-h-screen flex flex-col font-[family-name:var(--font-geist-sans)] items-center">
         <Navbar/>
-        <main className="flex-grow sm:w-2/3 w-full mx-auto flex flex-col items-center gap-y-4 text-xl mt-8">
+        <main className="flex-grow sm:w-2/3 mx-auto flex flex-col items-center gap-y-4 text-xl mt-8">
           <h1 className="text-center text-3xl font-semibold mb-4 border-b-2 border-white w-1/2 pb-2 inline-block">
             About Me
           </h1>
-          <div className="flex items-center space-x-10">
-            <div className="w-2/3 space-y-5">
+          <div className="sm:flex-row flex flex-col items-center sm:space-x-10 space-y-6">
+            <div className="flex w-1/2">
+              <img
+              src="ij-mozart.png"
+              alt="Relax Picture of Justin"
+              className={`object-cover rounded-2xl`}
+            />
+            </div>
+            <div className="w-1/2 space-y-5">
               <p>
                 A native of both South Korea and Georgia, Mr. Justin Han is a professional pianist, conductor, singer, 
                 and violinist in the Atlanta area. Justin is a graduate of Georgia State University, where he earned a 
@@ -28,14 +35,6 @@ export default function AboutPage() {
                 When he isn’t doing music, Justin enjoys cooking international cuisines and playing with his two cats, Presto 
                 and Melody.
               </p>
-            </div>
-            <div className="flex w-1/3">
-              <img
-              src="ij-mozart.png"
-              alt="Relax Picture of Justin"
-              className={`object-cover rounded-2xl"
-              }`}
-            />
             </div>
           </div>
         </main>
