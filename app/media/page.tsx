59d8dc05695e20a8f079be6ab5fd71150ca055ac
mzsellem <marleysellem@gmail.com>
2025-07-03 15:20:00 -0600

    remove navbar and footer from media page

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Navbar from '../Components/navbar'
-import Footer from '../Components/footer';
 
 export default function MediaPage() {
     return (
-      <main>
-        <Navbar/>
+      <div>
         <h1 className="text-3xl font-bold">Media</h1>
         <p>This is the Media page.</p>
-        <Footer />
-      </main>
+      </div>
     );
   }
