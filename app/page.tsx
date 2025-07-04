39328e7b005b452114c4b3acc0668b7ab5771b7b
mzsellem <marleysellem@gmail.com>
2025-07-04 10:18:51 -0600

    add apostrophe

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
         <div className="sm:w-1/2 w-full text-center sm:text-left space-y-4 text-md">
           <h1 className="text-3xl font-semibold mb-4 border-b-2 border-white w-full pb-2 inline-block">Testimonials</h1>
           <p className="mb-4">
-            The group`s new conductor, Justin Han, is taking the group in an exciting direction in his inaugural year. [He is] young, talented and ambitious.
+            The group's new conductor, Justin Han, is taking the group in an exciting direction in his inaugural year. [He is] young, talented and ambitious.
           </p>
           <p className="text-center sm:text-right">
             <a href="https://flagpole.com/music/music-features/2025/05/07/new-director-of-athens-master-chorale-brings-fresh-voices-and-concepts/?fbclid=IwY2xjawKPJW5leHRuA2FlbQIxMQBicmlkETExZ3NqUWJ3ME9nR1J0SHU0AR5xuhUpUVQXq4j3vlhNOEDgXuw1SOQOndUvsrgz2zItm9K50jTq-1lWR30PXg_aem_otdnw9aqmj2p7yEYSalfqg" target="_blank" rel="noopener noreferrer" className="hover:underline">
