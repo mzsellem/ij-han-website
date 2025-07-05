a0f282cc1181c54787ceee05755963e36823b5cf
mzsellem <marleysellem@gmail.com>
2025-07-05 10:03:22 -0600

    format home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,27 @@ const images = [
   "ij-baton-head.png",
 ];
 
+const testimonials = [
+  {
+    quote:
+      "The group's new conductor, Justin Han, is taking the group in an exciting direction in his inaugural year. [He is] young, talented and ambitious.",
+    author: {
+      name: "Flagpole Magazine Athens, GA",
+      link: "https://flagpole.com/music/music-features/2025/05/07/new-director-of-athens-master-chorale-brings-fresh-voices-and-concepts/?fbclid=IwY2xjawKPJW5leHRuA2FlbQIxMQBicmlkETExZ3NqUWJ3ME9nR1J0SHU0AR5xuhUpUVQXq4j3vlhNOEDgXuw1SOQOndUvsrgz2zItm9K50jTq-1lWR30PXg_aem_otdnw9aqmj2p7yEYSalfqg",
+    },
+  },
+  {
+    quote:
+      "With masterful precision and heartfelt expression, Justin IJ Han transforms every performance into a profound musical journey that resonates long after the final note.",
+    author: { name: "Alpharetta Symphony Orchestra" },
+  },
+  {
+    quote:
+      "Justin's leadership on the podium brings clarity, passion, and cohesion, elevating the ensemble to deliver performances of extraordinary depth and artistry.",
+    author: { name: "Emory Presbyterian Church" },
+  },
+];
+
 export default function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [fade, setFade] = useState(true);
@@ -24,42 +45,51 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="sm:ml-56 flex flex-col justify-center items-center p-8 min-h-screen">
-      <h1 className="text-2xl font-semibold">Justin "IJ" Han</h1>
-      <div className="flex flex-col sm:flex-row items-center justify-center gap-8 sm:px-12 py-8 max-w-7xl">
+    <> 
+    <main className="sm:ml-56 flex flex-col justify-center items-center p-15">
+      <div className="flex flex-col sm:flex-row items-center justify-center gap-20 sm:px-12 py-8 max-w-7xl">
         {/* Left column: Image Fade Carousel */}
-        <div className="sm:w-1/2 w-full overflow-hidden relative rounded-2xl">
-          <img
-            key={currentIndex}
-            src={images[currentIndex]}
-            alt="Three Scrolling pictures of Justin"
-            className={`w-full h-full object-cover rounded-2xl transition-opacity duration-3000 ${
-              fade ? "opacity-100" : "opacity-0"
-            }`}
-          />
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl font-semibold mb-5">Justin "IJ" Han</h1>
+          <div className="w-full max-w-md rounded-2xl">
+            <img
+              key={currentIndex}
+              src={images[currentIndex]}
+              alt="Three Scrolling pictures of Justin"
+              className={`w-full h-full object-cover rounded-2xl transition-opacity duration-3000 ${
+                fade ? "opacity-100" : "opacity-0"
+              }`}
+            />
+          </div>
         </div>
 
         {/* Right column: Text */}
-        <div className="sm:w-1/2 w-full text-center sm:text-left space-y-4 text-md">
-          <h1 className="text-3xl font-semibold mb-4 border-b-2 border-white w-full pb-2 inline-block">Testimonials</h1>
-          <p className="mb-4">
-            The group's new conductor, Justin Han, is taking the group in an exciting direction in his inaugural year. [He is] young, talented and ambitious.
-          </p>
-          <p className="text-center sm:text-right">
-            <a href="https://flagpole.com/music/music-features/2025/05/07/new-director-of-athens-master-chorale-brings-fresh-voices-and-concepts/?fbclid=IwY2xjawKPJW5leHRuA2FlbQIxMQBicmlkETExZ3NqUWJ3ME9nR1J0SHU0AR5xuhUpUVQXq4j3vlhNOEDgXuw1SOQOndUvsrgz2zItm9K50jTq-1lWR30PXg_aem_otdnw9aqmj2p7yEYSalfqg" target="_blank" rel="noopener noreferrer" className="hover:underline">
-              - Flagpole Magazine Athens, GA
-            </a>
-          </p>
-          <p className="mb-4">
-          With masterful precision and heartfelt expression, Justin IJ Han transforms every performance into a profound musical journey that resonates long after the final note.
-          </p>
-          <p className="text-center sm:text-right">- Alpharetta Symphony Orchestra</p>
-          <p>
-          Justin`s leadership on the podium brings clarity, passion, and cohesion, elevating the ensemble to deliver performances of extraordinary depth and artistry.          
-          </p>
-          <p className="text-center sm:text-right">- Emory Presbysterian Church</p>
+        <div className="sm:w-1/2 w-full flex flex-col space-y-8 text-xl text-left">
+          <h1 className="text-3xl font-semibold mb-4 border-b-2 border-white w-full pb-2 inline-block">
+            Testimonials
+          </h1>
+          {testimonials.map(({ quote, author }, idx) => (
+          <div key={idx}>
+            <p className="mb-2">{quote}</p>
+            <p className="text-center sm:text-left">
+              {author.link ? (
+              <a
+              href={author.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+              >
+              - {author.name}
+              </a>
+              ) : (
+              <>- {author.name}</>
+            )}
+            </p>
+          </div>
+        ))}
         </div>
       </div>
       </main>
+      </>
   );
 }
