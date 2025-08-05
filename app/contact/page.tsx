05fb17510ee000104134b908a4af97e458e3c7c2
mzsellem <marleysellem@gmail.com>
2025-08-05 15:03:03 -0600

    standardized page titles for contact, media, and upcoming

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,8 +4,8 @@ export default function ContactPage() {
     return (
       <main className="flex justify-center p-8 min-h-screen">
         <div className="flex-grow sm:w-2/3 w-full mx-auto flex flex-col items-center gap-y-4 text-xl mt-8">
-          <h1 className="text-center text-3xl font-semibold mb-4">
-            Get in Touch
+          <h1 className="text-3xl font-bold text-center mb-8 border-b-2 border-white w-fit mx-auto pb-2">
+            Contact
           </h1>
           <div className="sm:w-1/3 w-1/2 overflow-hidden relative rounded-2xl">
             <img
--- app/media/page.tsx
+++ app/media/page.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function MediaPage() {
     return (
       <main className="flex justify-center p-8 min-h-screen">
-        <div>
-          <h1 className="text-3xl font-bold">Media</h1>
+        <div className="min-h-screen px-4 py-8 font-[family-name:var(--font-geist-sans)]">
+          <h1 className="text-3xl font-bold text-center mb-8 border-b-2 border-white w-fit mx-auto pb-2">Media</h1>
           <p>This is the Media page.</p>
         </div>
       </main>
