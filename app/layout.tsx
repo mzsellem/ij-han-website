46ec4ecca93cdf026a762c177cd7d027180bdc6e
mzsellem <marleysellem@gmail.com>
2025-08-15 09:20:52 -0600

    change font and add padding on page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,14 @@ import type { Metadata } from "next";
 import React from "react";
 import "./globals.css";
 import Navbar from "./Components/Navbar";
+import { Merriweather } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: "IJ-Han-Website",
   description: "Portfolio Website of IJ Han",
 };
+
+const merriweather = Merriweather({ subsets: ['latin'], weight: ['400', '700'] });
 
 export default function RootLayout({
   children,
@@ -14,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={merriweather.className}>
       <body className="min-h-screen flex bg-[var(--background)]">
         {/* Sidebar Navbar */}
           <Navbar />
@@ -22,8 +25,6 @@ export default function RootLayout({
         <div className="flex-1 ml-0 sm:ml-56 overflow-x-hidden">
           {children}
         </div>
-        {/* Optional: Footer outside sidebar */}
-        {/* <Footer /> */}
       </body>
     </html>
   );
